466b9f7de365d931a9910d745eaa51f0997afa38
Matt Valentine <mattvalentine@gmail.com>
2020-09-04 22:07:38 -0400

    big red screen with nothing on it

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Screen />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Screen from "./components/Screen.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Screen,
+  },
+};
 </script>
 
 <style>
+body {
+  margin: 0px;
+  background: black;
+  box-sizing: border-box;
+  color: white;
+  height: 100vh;
+  width: 100vw;
+  /* display: flex; */
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
 }
 </style>
---
+++ src/components/Screen.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="screen">Screen Component</div>
+</template>
+
+<script>
+export default {
+  name: "Screen",
+};
+</script>
+
+<style>
+.screen {
+  background: red;
+  width: 100%;
+  height: 100%;
+}
+</style>
